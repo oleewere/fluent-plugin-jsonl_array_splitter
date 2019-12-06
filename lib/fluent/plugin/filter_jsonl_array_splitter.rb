51e58a33bbea74af35c5b4f4c806b7810d9e917b
Oliver Szabo <oleewere@gmail.com>
2019-12-07 00:43:00 +0100

    Init jsonl splitting

--- lib/fluent/plugin/filter_jsonl_array_splitter.rb
+++ lib/fluent/plugin/filter_jsonl_array_splitter.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module Fluent
     module Plugin
         class JsonlArraySplitterFilter < Fluent::Plugin::Filter
@@ -7,8 +9,41 @@ module Fluent
             config_param :key_name, :string
             desc "Keep original key in parsed result."
             config_param :reserve_key, :bool, default: false
-            
 
+            def filter_stream(tag, es)
+                new_es = Fluent::MultiEventStream.new
+                es.each do |time, record|
+                    record[@key_name].each do |r|
+                        new_record = record.clone
+                        new_record.update(r)
+
+                        if !@reserve_key then
+                            new_record.delete(@key_name)
+                        end
+                        new_es.add(time, new_record)
+                    end
+                end
+                new_es
+            end
+
+            def jsonl_generate(objs, opts = nil)
+                unless objs.is_a?(Array)
+                    raise TypeError, "can't generate from #{objs.class}"
+                end
+                generated = []
+                objs.map do |obj|
+                  generated << JSON.generate(obj, opts)
+                end
+                generated.join("\n")
+            end
+
+            def jsonl_parse(source, opts = {})
+                parsed = []
+                source.each_line do |line|
+                    parsed << JSON.parse(line, opts)
+                end
+                parsed
+            end
         end
     end
 end
